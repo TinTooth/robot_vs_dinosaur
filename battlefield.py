a99374e9e160f62557b7a16d1c80d6392488a488
TinTooth <boconne4@gmail.com>
2022-09-27 11:08:05 -0500

    Final Version. output refactoring done

--- battlefield.py
+++ battlefield.py
@@ -11,55 +11,184 @@ class BattleField:
         self.fleet = Fleet(data.robots)
         self.robot = 'TBD'
         self.herd = Herd(data.dinosaurs)
+        self.dinosaur = 'TBD'
+       
+    def check_fighters_health(self):
+        if self.robot.health > 0 and self.dinosaur.health > 0:
+            self.display_health()
+            time.sleep(1)
+            return
+        if self.robot.health <= 0:
+            self.fleet.fleetsize -= 1
+        if self.dinosaur.health <= 0:
+            self.herd.herdsize -= 1
+        if self.robot.health <=0 and self.fleet.fleetsize > 0:
+            print(f'{self.robot.name} was Defeated!')
+            time.sleep(2)
+            self.choose_robot()
+            self.display_fighters("Your next Robot is Ready to GO!")
+            self.display_health()
+        if self.dinosaur.health <= 0 and self.herd.herdsize > 0:
+            print(f'{self.dinosaur.name} was Defeated!')
+            time.sleep(2)
+            self.choose_active_dinosaur()
+            self.display_fighters("The next Dinosaur Steps into the Ring!")
+            self.display_health() 
+            
+    def choose_active_dinosaur(self):
         self.dinosaur = random.choice(self.herd.dinosaurs)
         self.herd.dinosaurs.remove(self.dinosaur)
 
     def choose_robot(self):
-        i = 1
-        for robot in self.fleet.robots:
-            print(f'{i}) {robot.name} ')
-            i +=1
-        choice = (input('Pick Your Starting Robot!  '))
+        self.display_fleet('Your Fleet')
+        choice = input('Pick your Fighter!  ')
         choice = self.validate_num_input(choice,len(self.fleet.robots)+1)
         self.robot = self.fleet.robots[int(choice) -1]
         self.fleet.robots.remove(self.robot)
+
+    def choose_num_of_fighters(self):
+        print()
+        user_input = input('How many Fighters per Team? 1, 2 or 3:  ')
+        print()
+        user_input = self.validate_num_input(user_input,4)
+        if user_input == '1':
+            self.fleet.fleetsize -= 2
+            self.herd.herdsize -= 2
+            print('    1 ROBOT VS 1 DINOSAUR!')
+        elif user_input == '2':
+            self.fleet.fleetsize -= 1
+            self.herd.herdsize -= 1
+            print('    2 ROBOTS VS 2 DINOSAURS!')
+        else:
+            print('    3 ROBOTS VS 3 DINOSAURS!')
+
+    def display_dinosaurs(self):
+        print('The Herd of Dinosaurs: The TRI FORCE')
+        i = 1
+        for dinosaur in self.herd.dinosaurs:
+            print(f'{i}) {dinosaur.name}')
+            i += 1
+        
+    def display_fleet(self,prompt):
+        i = 1
+        print()
+        print(prompt)
+        for robot in self.fleet.robots:
+            print(f'{i}) {robot.name} ')
+            i +=1
 
     def display_fighters(self,prompt):
         print()
         print(prompt)
         print(f'{self.robot.name} VS {self.dinosaur.name}')
         time.sleep(2)
-
-    def display_welcome_start_game(self):
+    
+    def display_health(self):
         print()
-        print('       Welcome to Robots vs Dinosaurs!')
-        self.choose_num_of_fighters()
-        self.display_fighters("The First Fighters!")
-        self.display_health()
-      
-
-    def choose_num_of_fighters(self):
-        user_input = input('How many Fighters per Team? 1, 2 or 3:  ')
-        user_input = self.validate_num_input(user_input,4)
-        self.choose_robot()
-        if user_input == '1':
-            self.fleet.fleetsize -= 2
-            self.herd.herdsize -= 2
-        elif user_input == '2':
-            self.fleet.fleetsize -= 1
-            self.herd.herdsize -= 1
+        print(f'           {self.robot.name} has {self.robot.health} Health  ', '/',f'  {self.dinosaur.name} has {self.dinosaur.health} Health')
 
     def display_weapons(self):
         i = 1
         for weapon in self.robot.weapons:
             print(f'{i}) {weapon.name}   Attack Power:  {weapon.attack_power}  Type:  {weapon.type}')
             i += 1
-
-    def display_health(self):
+            
+    def display_welcome_start_game(self):
         print()
-        print(f'           {self.robot.name} has {self.robot.health} Health  ', '/',f'  {self.dinosaur.name} has {self.dinosaur.health} Health')
-        # print(f'{self.dinosaur.name} has {self.dinosaur.health} Health') 
+        print('       Welcome to Robots vs Dinosaurs!')
+        print()
+        self.display_dinosaurs()
+        self.choose_active_dinosaur()
+        self.choose_robot()
+        self.choose_num_of_fighters()
+        self.display_fighters('The First Fighters')
+        self.display_health()
         
+    def display_winner(self):
+        print()
+        if self.herd.herdsize == 0:
+            print(f'{self.dinosaur.name} was Defeated!')
+            print()
+            print(f'The Robots are VICTORIOUS!!')
+            self.print_robot()
+        else:
+            print(f'{self.robot.name} was Defeated!')
+            print()
+            time.sleep(2)
+            print(f'The Dinosaurs are VICTORIOUS!!')
+            self.print_dinosaur()
+        print()
+
+    def select_weapon(self):
+        self.display_weapons()
+        user_input = input('Select a Weapon! 1, 2 or 3:  ')
+        user_input = self.validate_num_input(user_input,4)
+        self.robot.active_weapon = self.robot.weapons[int(user_input)-1]
+        self.dinosaur.active_attack = random.choice(self.dinosaur.attacks)
+            
+    def print_dinosaur(self):
+
+        print( '                                                  00000000000000                                     ' )    
+        print( '                                               00               00000000                       ' )    
+        print( '                                              00    (XXxx)               00                        ' )    
+        print( '                                              00                         00                      ' )    
+        print( '                                              00                 0 0000000                      ' )    
+        print( '                                              00          00 0000    V V                         ' )    
+        print( '                                              00                 0000,                             ' )    
+        print( '                                              00          0 00 0 000,                              ' )    
+        print( '                                              00         00                              ' )    
+        print( '               00                     00000000         00                                 ' )    
+        print( '               00                  000                  00 11111                                 ' )    
+        print( '               0000            00000       11             00    11                                   ' )    
+        print( '               0000          0000           11            00    11                                ' )    
+        print( '               00000000000000                VVV          00   VV                                       ' )    
+        print( '               00                                       000                                 ' )    
+        print( '                00                                       00                                ' )    
+        print( '                  00 000                                 00                                   ' )    
+        print( '                        000                000           00                                    ' )    
+        print( '                           000           000 000       00                                         ' )    
+        print( '                              000 00000 000     000   00                                               ' )    
+        print( '                                  0000            000 0                                       ' )    
+        print( '                                  000             000                                       ' )    
+        print( '                                  00 0            000                                      ' )    
+        print( '                                  00  0           00 0                                       ' )    
+        print( '                                  0 00 0          000 0                                        ' )    
+        print( '  ')           
+
+    def print_robot(self):
+        print('                                                                               ')
+        print('                            00000                                                ')
+        print('                         00       00                                              ')
+        print('                          00     00                                            ')
+        print('                            00 00                                                 ')
+        print('                              0                                                 ')
+        print('                              0                                                 ')
+        print('               00000000000000000000000000000000                                                      ')
+        print('               0                              0                               ')
+        print('               0     0 0 0 0      0 0 0 0     0         000 00          00 000               ')
+        print('               0      0 0 0        0 0 0      0         0     0        0     0       ')
+        print('               0     0 0 0 0      0 0 0 0     0         0      0      0      0         ')
+        print('               0                              0          0      000000      0     ')
+        print('               0    1111111111111111111111    0           0               0                       ')
+        print('               0     1                    1   0            0 000     000 0        ')
+        print('               0      1                  1   0                  0    0         ')
+        print('               0       11111111      111    0                   0   0                  ')
+        print('                0              111111      0                     0 0                ')
+        print('                  0                      0                       0 0         ')
+        print('                    0 0000000000000000000          0 0 0000      0 0                           ')
+        print('                          00000000                0        0    0    0            ')
+        print('          00000000000000000000000000000000000000000         0  0     0                                              ')
+        print('          0                                       0           V       0      ')
+        print('          0                                       0                  0       ')
+        print('          0                                       0  00 000 0000  000                     ')
+
+    def validate_num_input(self,choice,end_of_range):
+        while True:
+            if choice.isnumeric() and int(choice) in range(end_of_range):
+                break
+            else:
+                choice = input('Please enter a correct number:  ')
+        return choice
 
     def battle_phase(self):
         self.select_weapon()
@@ -91,57 +220,6 @@ class BattleField:
                 else:
                     self.dinosaur.attack(self.robot)
                 time.sleep(2)
-       
-    def display_winner(self):
-        print()
-        if self.herd.herdsize == 0:
-            print(f'{self.dinosaur.name} was Defeated!')
-            print()
-            time.sleep(2)
-            print(f'The Robots are VICTORIOUS!!')
-        else:
-            print(f'{self.robot.name} was Defeated!')
-            print()
-            time.sleep(2)
-            print(f'The Dinosaurs are VICTORIOUS!!')
-        print()
-            
-    def validate_num_input(self,choice,end_of_range):
-        while True:
-            if choice.isnumeric() and int(choice) in range(end_of_range):
-                break
-            else:
-                choice = input('Please enter a correct number:  ')
-        return choice
-
-    def select_weapon(self):
-        self.display_weapons()
-        user_input = input('Select a Weapon! 1, 2 or 3:  ')
-        user_input = self.validate_num_input(user_input,4)
-        self.robot.active_weapon = self.robot.weapons[int(user_input)-1]
-        self.dinosaur.active_attack = random.choice(self.dinosaur.attacks)
-
-    def check_fighters_health(self):
-        if self.robot.health > 0 and self.dinosaur.health > 0:
-            self.display_health()
-            time.sleep(1)
-        if self.robot.health <= 0:
-            self.fleet.fleetsize -= 1
-        if self.dinosaur.health <= 0:
-            self.herd.herdsize -= 1
-        if self.robot.health <=0 and self.fleet.fleetsize > 0:
-            print(f'{self.robot.name} was Defeated!')
-            time.sleep(2)
-            self.choose_robot()
-            self.display_fighters("The next Fighter!")
-            self.display_health()
-        if self.dinosaur.health <= 0 and self.herd.herdsize > 0:
-            print(f'{self.dinosaur.name} was Defeated!')
-            time.sleep(2)
-            self.dinosaur = random.choice(self.herd.dinosaurs)
-            self.herd.dinosaurs.remove(self.dinosaur)
-            self.display_fighters("The next Fighter!")
-            self.display_health() 
 
     def run_game(self):
         self.display_welcome_start_game()
@@ -155,12 +233,3 @@ class BattleField:
                 i = i+1    
             time.sleep(1)  
         self.display_winner() 
-            
-
-
-    
-
-
-
-
-                
